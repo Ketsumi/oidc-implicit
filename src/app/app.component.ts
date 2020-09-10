792ab718ca49a22e5eae9dd8fdd8ac7ad1f616c6
Ketsumi <mewtropolis.ketsumi@gmail.com>
2020-09-10 03:06:25 +0000

    components created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'oidc-implicit';
+  public title: string;
+  public time: string;
+
+  constructor() {
+    this.title = 'oidc-implicit';
+    this.time = new Date().toISOString();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CallbackComponent } from './callback/callback.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    CallbackComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
