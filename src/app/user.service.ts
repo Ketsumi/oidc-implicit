e6296acd4ae36ce43aecbb1cdaa9b2e90d864533
Ketsumi <mewtropolis.ketsumi@gmail.com>
2020-09-10 06:26:32 +0000

    wrote user service and class

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  constructor(private users:User[]) { }
 
-  constructor() { }
+  public push(user: User) {
+    this.users.push(user);
+  }
+
+  public pop() {
+    this.users.pop();
+  }
+
+  public getUsers() {
+    return this.users;
+  }
 }
