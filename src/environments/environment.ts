ae4808919ff702b3a8747722992df1ccd5c4b6e9
Ketsumi <mewtropolis.ketsumi@gmail.com>
2020-09-16 08:08:22 +0000

    environment changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,10 +8,10 @@ export const environment = {
     google: {
       url: 'https://accounts.google.com/o/oauth2/v2/auth',
       params: {
-        response_type: 'id_token',
-        client_id: '986484840298-lg8and8ts23n47cgs1thkgk5a2uroge0.apps.googleusercontent.com',
-        scope: 'openid email',
-        redirect_uri: 'https://ketsumi.github.io/oidc-implicit/callback'
+        response_type: '',
+        client_id: '',
+        scope: '',
+        redirect_uri: ''
       }
     }
   }
